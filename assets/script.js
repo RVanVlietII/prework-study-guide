81792df9724b3121eb5244952a3cf5aea0d11620
BigTimeRoby <rcvanvlietii@gmail.com>
2023-05-01 10:50:06 -0500

    <JavaScript notes added>

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,26 @@
-var topics = ['HTML','CSS', 'Git', 'JavaScript'];
+var Topics = ['HTML','CSS', 'Git', 'JavaScript'];
+var randomTopic = Topics[Math.floor(Math.random() * Topics.length)];
 
-for(var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
+function listTopics() {
+    for(var x = 0; x < Topics.length; x++) {
+        console.log(Topics[x]);
+    }
 }
 
-/*
-if (topic === 'HTML'){
+function selectTopic() {
+    if (randomTopic === 'HTML'){
     console.log("Let's study HTML!");
-} else if (topic === 'CSS') +{
+    } else if (randomTopic === 'CSS') {
     console.log("Let's study CSS!");
-} else if (topic === 'JavaScript') {
+    } else if (randomTopic === 'JavaScript') {
     console.log ("Let's study JavaScript!");
-} else {
+    } else {
     console.log('Please try again!');
+    }
 }
-*/
+
+console.log('Here are the topics we learned through Prework:');
+listTopics()
+
+console.log('Which topic should we study first?');
+selectTopic()
